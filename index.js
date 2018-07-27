cce40a5c9b42fd78da3ac2dcf76aaead3baf4078
Peixe <thefish0987@hotmail.com>
2018-07-27 09:12:39 -0300

    Segundo commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/*Código resumido - feito pelo professor em aula*/
+
+const fs = require('fs');
+const comando = process.argv[2];
+const nome = process.argv[3];
+const email = process.argv[4];
+
+let cadastros = {};
+
+fs.readFile('cadastros.json',  (err, dados) => {
+
+    if(!err){
+        let dadosString = dados.toString();
+        cadastros = JSON.parse(dadosString);
+    }
+    
+    if(comando === 'salvar'){
+        cadastros[nome] = email;
+    
+        const dadosGravacao = JSON.stringify(cadastros);
+    
+        fs.writeFile('cadastros.json', dadosGravacao, (err) => {
+            if(err){
+                console.log('Deu ruim');
+            }else{
+                console.log('Gravei o arquivo');
+            }
+        });
+    
+    }else if(comando === 'buscar-todos'){
+        console.log(cadastros);
+    }else if(comando == 'buscar'){
+        console.log(cadastros[nome]);
+    }else{
+        console.log('Tá bebo?');
+    }
+    
+});
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+A -  Os console.log(Nota: ...) foram criados para verificar se o código está reagindo aos casos apenas.
+
+B - Abrir o CMD para executar e digitar:
+    Criar novo nome e email: node script.js salvar Luis luis@uol
+    Buscar email digitando nome: node script.js buscar Luis
+    Escrever todos os nomes e emails: node script.js buscar-todos */
+
 //1 -Para pegar os arquivos do pc
 const fs = require("fs")
 
@@ -8,19 +16,45 @@ const email = process.argv[4];
 
 //4 - Cria-se uma variável para salvar os cadastros em objeto, pois NODE lê em um objeto.
 
-const cadastros = {};
+let cadastros = {};
 
-// 3 - Tratando as respostas dadas aos inputs criou-se os Ifs abaixo.
-if(comando === "salvar"){
+// 7 - Carrega o arquivos "cadastros.JSON (?) 
+
+fs.readFile("cadastros.json", (err,dados) => {
     
-    console.log("Comando SALVAR");
-}
-else if(comando === "buscar-todos"){
-    console.log("Comando foi BUSCAR TODOS");
-}
-else if(comando === "buscar"){
-    console.log("Comando foi BUSCAR");
-}
-else{
-    console.log("Digite um comando existente.")
-}
+    //8 - Corrigir erro: Caso não tenha um arquivo cadastro.json criado:
+    if(!err){
+        let dadosString = dados.toString(); // 5.1 Transforma em String para conseguir ler
+        cadastros = JSON.parse(dadosString);
+    }    
+    
+    // 3 - Tratando as respostas dadas aos inputs criou-se os Ifs e Else Ifs abaixo.
+    if(comando === "salvar"){
+        cadastros[nome] = email; //4.1 A ideia é tornar o nome a chave e o email da pessoa o elemento.
+        
+        const dadosGravacao = JSON.stringify(cadastros) //5 Converter cadastros de object para string para poder passar para o JSON e evitar o erro object:Object
+        
+        fs.writeFile("cadastros.json", dadosGravacao, (err) => { //6 - 
+            if(err){
+                console.log("Deu  ruim");
+            }
+            else{
+                console.log("ARQUIVO GRAVADO")
+            }
+        });
+        console.log("Nota: Comando SALVAR");
+    }
+    else if(comando === "buscar-todos"){
+        console.log(cadastros)//4.3 Mostra todos os elementos registrados em cadastros.
+        console.log("Nota: Comando foi BUSCAR TODOS");
+    }
+    else if(comando === "buscar"){
+        console.log(cadastros[nome]); //4.2 Mostra email do nome registrado na chave (que é o nome buscado)
+        console.log("Nota: Comando foi BUSCAR");
+    }
+    else{
+        console.log("Nota: Digite um comando existente.")
+    }    
+    
+});
+
